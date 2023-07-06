10cd415a0e19f7e127b3215059633a1584803346
git config --global user.name rodneaa <136031276+rodneaa@users.noreply.github.com>
2023-07-06 12:52:20 -0400

    Update pypoll_practice.py
    if statement ideas

--- pypoll_practice.py
+++ pypoll_practice.py
@@ -1,6 +1,10 @@
 import os
 import csv
 
+
+# ? from collections import counter
+
+
 #open csv
   csvpath = os.path.join("..", "python-challenge", "Pypoll", "Resources", "election_data.csv")
 
@@ -14,51 +18,47 @@ import csv
 
 #### The total number of votes cast (either column)
 total_votes = 0
+    for row in csvfile
     total_votes = [int(row[1]) for row in data]
     print(f'Total Votes: ', total_votes)
 
-### A complete list of candidates who received votes (Column 3)
-    result = {}
+### A complete list of candidates who received votes, # and % of votes recd (Column 3)
+    candidate_votes=[total_votes.count(candidate),  for candidate in candidates in set (candidates)]   #youtube caleb curry, Pyhton tutorial 10
+    print(candidate, vote_counts)
 
-    for row in csvreader:
-        if row[1] in result:
-            result[row[2]].append(row[1])
-        else:
-            result[row[2]] = [row[1]]
-    print result
+
+      
+    row =[ballotID, County, candidate]
+    result = {}
+    candidate = [row[2]   #column3
+    candidates = []   #number of different variables (candidates) in column 3
+    candidate_votes = 0  #number of times each variable appears
+    
+    for candidate_votes in total_votes:
+        if candidate in candidates = candidate:
+         candidate_votes = candidate_votes + 1
+          else 
+          candidate_votes = 1
+
+
+        totalPurchases = totalPurchases + 1;
+        totalSum = totalSum + price;
+        if(price > 1800):
+            sumAbove1800 = sumAbove1800 + price;
+            countAbove1800 = countAbove1800 + 1;
+
+
+
+
+
+
+### The total number of votes each candidate won ** candidate_votes
+
+### The percentage of votes each candidate won is **each candidate divided by the total number of votes recd**
+
+
+### The winner of the election based on popular vote ****max(candidate_votes)
+
 
 ----------------------------------------------------
-#https://stackoverflow.com/questions/50850093/group-corresponding-rows-value-in-a-columns-python?noredirect=1&lq=1
-ans = dict()
-for n, row in enumerate(reader):
-    if n == 0:
-        titles = row
-    else:
-        num = row[0]
-        if num in ans:
-            t = ans[num]
-            for i in range(1, len(row)):
-                t[i].append(row[i])
-        else:
-            tmp = []
-            for other in row:
-                tmp.append([other])
-            ans[num] = tmp
-
-writer = csv.writer(open("2.csv", 'w'))
-writer.writerow(titles)
-for row in ans.values():
-    tmp = []
-    for key in row:
-        if isinstance(t, str):
-            tmp.append(key)
-        else:
-            tmp.append(",".join(key))
-    writer.writerow(tmp)
-
-  
-### The percentage of votes each candidate won
-
-### The total number of votes each candidate won
-
-### The winner of the election based on popular vote
+#https://stackoverflow.com/questions/50850093/group-corresponding-rows-value-in-a-columns-python?noredirect=1&lq=1 ca
